7c897545b03f4fe03782715979e0cf2400a92897
Anderlozu1 <108639413+Anderlozu1@users.noreply.github.com>
2022-12-14 20:00:15 -0500

    Agregar .gitignore y primeros archivos del proyecto

---
+++ a.js
@@ -0,0 +1,9 @@
+[1, 2, 3].push(4);        // Aumenta 1 al final:              [1,2,3,4]
+[1, 2, 3].pop();         // Quita el ultimo:                  [1,2]
+[1, 2, 3].shift();       // Quita el primero:                 [2,3]         Se puede guardar en una var
+[1, 2, 3].unshift(0);    // Aumenta 1 al inicio (con valor):  [0,1,2,3]
+['a', 'b'].concat('c');  //Concatena un STRING:               ['a','b','c']
+['a', 'b', 'c'].join('-'); // Inserta en medio de elementos   [a-b-c]
+['a', 'b', 'c'].slice(1); //                                  ['a','b']
+['a', 'b', 'c'].indexOf('b'); // Muestra la ubicación         1
+[]
